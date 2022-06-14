8c564d5788dbfd5d83e0c20d66d3fb1a6bc6b449
Your Name <you@example.com>
2022-06-14 11:37:04 +0430

    redundant get_opt removed

--- detect.py
+++ detect.py
@@ -218,31 +218,7 @@ def detect(save_img=False):
     return ('Done. (%.3fs)' % (time.time() - t0))
 
 
-def get_opt():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--cfg', type=str, default='cfg/yolov3-spp.cfg', help='*.cfg path')
-    parser.add_argument('--names', type=str, default='/home/allen/Desktop/yolo/YOLO_V3_TRAIN/yolov3/data/humanface.names', help='*.names path')
-    parser.add_argument('--weights', type=str, default='/home/allen/Desktop/yolo/YOLO_V3_TRAIN/yolov3/weights/best.pt', help='weights path')
-    parser.add_argument('--source', type=str, default='/home/allen/Downloads/track2.2_test_sample', help='source')  # input file/folder, 0 for webcam
-    parser.add_argument('--output', type=str, default='output', help='output folder')  # output folder
-    parser.add_argument('--img-size', type=int, default=512, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.6, help='IOU threshold for NMS')
-    parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
-    parser.add_argument('--half', action='store_true', help='half precision FP16 inference')
-    parser.add_argument('--device', default='', help='device id (i.e. 0 or 0,1) or cpu')
-    parser.add_argument('--view-img', action='store_true', help='display results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    opt = parser.parse_args()
-    return opt
-
-
 if __name__ == '__main__':
-    opt = get_opt()
-    print(opt)
 
     with torch.no_grad():
-        detect(opt)
+        detect()
