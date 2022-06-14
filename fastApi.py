8eb130ed8aacffa6aba6a2d1c240dc56edff4e51
Your Name <you@example.com>
2022-06-14 14:55:12 +0430

    crash problem fixed by disabled gradiant in inferencec

--- fastApi.py
+++ fastApi.py
@@ -1,11 +1,13 @@
-import detect
+from detect import detect
 from fastapi import FastAPI
-
+import torch
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    result = detect.detect()
-    return {"message1": result}
+
+   with torch.no_grad():
+       result = detect()
+       return {"message1": result}
