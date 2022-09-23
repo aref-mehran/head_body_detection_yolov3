844880ecce99fe5a3223f066ee4d6daf63a72c41
Your Name <you@example.com>
2022-09-23 08:38:35 +0000

    init.sh updated to download best.pt in google cloud

--- src/init.sh
+++ src/init.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+pip install gdown
+mkdir $SCRIPT_DIR/weights
+gdown 12Rq-pTAPz5ulxnWGZa6h1JxgRVBr5tc8 -O $SCRIPT_DIR/weights/best.pt
 pip install fastapi[all]
 uvicorn fastApi:app  --host 0.0.0.0 --port 8000 --reload
 #dfdfdfdf
