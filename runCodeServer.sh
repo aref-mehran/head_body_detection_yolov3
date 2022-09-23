fb16a55a7398e4d7f897a20566ced759af7e5a66
Your Name <you@example.com>
2022-09-23 09:05:34 +0000

    runCodeServer added to use vscode in colab

---
+++ runCodeServer.sh
@@ -0,0 +1,5 @@
+wget https://github.com/coder/code-server/releases/download/v4.7.0/code-server_4.7.0_amd64.deb
+sudo dpkg -i code-server_4.7.0_amd64.deb
+rm code-server_4.7.0_amd64.deb
+export PASSWORD="92722138"
+code-server
